50e5c0353578fba27a7490a3dfad8fe53441b962
wejpurvis <williampurvis00@gmail.com>
2023-12-10 17:54:23 +0000

    updated utils unit tests

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,17 @@
 """
 | This module contains utility functions that are used throughout the project.
-| :code:`parse_grid(sudoku)` converts a text-based suduko grid into a numpy array
+| :code:`parse_grid()` converts a text-based suduko grid into a numpy array
    ready for processing.
-  :code:`isValidSudoku(board)` checks if a sudoku board is valid (*note*: a valid sudoku
-   does not necessarily mean that the sudoku is solvable).
-  :code:`display_sudoky(board)` converts a list of lists into a text-based
+  :code:`validate_board()` checks if a sudoku board is valid (*note*: a valid sudoku
+   does not necessarily mean that the sudoku is solvable). If the board is invalid, the
+    row and column of the invalid cells are returned.
+  :code:`highlight_errors()` highlights the invalid cells in red.
+  :code:`display_sudoku()` converts a list of lists into a text-based
    suduko grid for display purposes.
 """
 
 import numpy as np
+from typing import Union
 
 
 def parse_grid(sudoku: str) -> np.array:
@@ -78,9 +81,9 @@ def parse_grid(sudoku: str) -> np.array:
     return sudoku_array
 
 
-def isValidSudoku(board: list[list[int]]) -> bool:
+def validate_board(sudoku: np.array) -> Union[bool, list[tuple[int, int]]]:
     """
-    Given a list of lists represrenting a sudoku board, check if the board is valid.
+    Given a 2D numpy array representing a sudoku board, check if the board is valid.
     Validity of sudoku is based off the following rules:
 
     1) Each row must contain the digits 1-9 without repetition.
@@ -90,54 +93,43 @@ def isValidSudoku(board: list[list[int]]) -> bool:
 
     *Note*: a valid sudoku does not necessarily mean that the sudoku is solvable.
 
+    If the board is valid, True is returned. If the board is invalid, False and the row
+    and column of the invalid cells are returned.
+
     Parameters
     ----------
-    board : list[list[int]]]
-        List of list where elements can be accessed by row [i] & column [j]
+    sudoku : np.array
+        2D numpy array representing a sudoku board
 
     Returns
     ---------
     bool
         True if board is valid, False otherwise
+    list[tuple[int, int]]
+        List of tuples containing the row and column of the invalid cells. Empty list
+        if board is valid.
 
     Raises
     ---------
     TypeError
-        If input is not a list of lists of ints
-    ValueError
-        If input is not 9x9 or if cells contains values outside of 0-9
-
+        If input is not a numpy array
     """
+    # Check if input is valid (type)
+    if type(sudoku) != np.ndarray:
+        raise TypeError("Input must be a numpy array")
 
-    # Check if input is valid (list of list)
-    if type(board) != list:
-        raise TypeError("Input must be a list")
-    if type(board[0]) != list:
-        raise TypeError("Input must be a list of lists")
-
-    # Check if input is valid (length)
-    if len(board) != 9:
-        raise ValueError("Input grid must be 9x9")
-    for row in board:
-        if len(row) != 9:
-            raise ValueError("Input grid must be 9x9")
-
-    # Check if input is valid (values)
-    for row in board:
-        for val in row:
-            if type(val) != int:
-                raise TypeError("Input must be a list of lists of ints")
-            if val < 0 or val > 9:
-                raise ValueError("Input must be a list of lists of ints from 0-9")
+    # Convert numpy array to list of lists
+    sudoku_list = [list(row) for row in sudoku]
 
     # Check validity of sudoku
     rows = [set() for _ in range(9)]
     cols = [set() for _ in range(9)]
     block = [[set() for _ in range(3)] for _ in range(3)]
+    invalid_cells = []
 
     for i in range(9):  # iterate over rows
         for j in range(9):  # iterate over columns
-            element = board[i][j]
+            element = sudoku_list[i][j]
             if element == 0:
                 continue
             if (
@@ -145,11 +137,54 @@ def isValidSudoku(board: list[list[int]]) -> bool:
                 or (element in cols[j])
                 or (element in block[i // 3][j // 3])
             ):
-                return False
+                invalid_cells.append((i, j))
             rows[i].add(element)
             cols[j].add(element)
             block[i // 3][j // 3].add(element)
-    return True
+
+    if len(invalid_cells) == 0:
+        return True
+    else:
+        return False, invalid_cells
+
+
+def highlight_errors(sudoku: np.array, invalid_cells: list[tuple[int, int]]) -> None:
+    """
+    Given a 2D numpy array representing a sudoku board and a list of invalid cells for
+    the given board, highlight the invalid cells in red.
+
+    Parameters
+    ----------
+    sudoku : np.array
+        2D numpy array representing a sudoku board
+    invalid_cells : list[tuple[int, int]]
+        List of tuples containing the row and column of the invalid cells.
+
+    """
+    # Define ANSI escape codes for text colours
+    RED = "\033[91m"
+    RESET = "\033[0m"
+
+    # Convert numpy array to list of lists
+    sudoku_list = [list(row) for row in sudoku]
+
+    # Convert list of lists into a string
+    sudoku_str = ""
+    for row in sudoku_list:
+        for idx, val in enumerate(row):
+            # Highlight invalid cells in red
+            if (sudoku_list.index(row), idx) in invalid_cells:
+                char_str = f"{RED}{val}{RESET}"
+            else:
+                char_str = str(val)
+            if idx == 2 or idx == 5:
+                sudoku_str += char_str + "|"
+            else:
+                sudoku_str += char_str
+        sudoku_str += "\n"
+        if row == sudoku_list[2] or row == sudoku_list[5]:
+            sudoku_str += "---+---+---\n"
+    print(sudoku_str)
 
 
 def display_sudoku(board: list[list[int]]) -> str:
