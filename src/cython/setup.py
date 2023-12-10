80f826646149c7b4d58b9d27c8ab4dce0d5b44d0
wejpurvis <williampurvis00@gmail.com>
2023-12-10 14:40:30 +0000

    keep cython docstrings

--- src/cython/setup.py
+++ src/cython/setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
-from Cython.Build import cythonize
+from setuptools import Extension
+from Cython.Distutils import build_ext
+
+ext_modules = [Extension("bt_mrv", ["bt_mrv.pyx"])]
+
+for e in ext_modules:
+    e.cython_directives = {"embedsignature": True}
 
 setup(
     name="Cythonised backtracking with MRV which takes c arrays",
-    ext_modules=cythonize("bt_mrv.pyx"),
+    cmdclass={"build_ext": build_ext},
+    ext_modules=ext_modules,
 )
 
 # run python setup.py build_ext --inplace
